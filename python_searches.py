40606e74208955c96c0c9ccab53862d453f715ab
Dave Edwards <EdwardsD@hlcollege.ac.uk>
2022-11-25 12:41:57 +0000

    A few comments and a test for a BS

--- python_searches.py
+++ python_searches.py
@@ -13,21 +13,12 @@ def binary_search(search_item, items):
     end_index = 9999
     
     # loop as long as subarray is not empty
-    while start_index <= 9999:
-        # mid_index is int average of start_index and end_index
-        mid_index = 9999
-        # set current_item to the item in the middle
-        current_item = items[9999]
-        # if current item is what we're looking for return True
-        if current_item == search_item:
-            return 9999
-        # otherwise update the start_index or end_index depending on 
-        # which section of the array can be discarded.
-        elif current_item > search_item:
-            ? = ?
-        else:
-            ? = ?
-        # return False when you are sure that the search_item is not in items
+    # mid_index is int average of start_index and end_index        
+    # set current_item to the item in the middle
+    # compare current_item to search item, and return True if found
+    # or update the start_index or end_index depending on 
+    # which section of the array can be discarded.
+    # return False when you are sure that the search_item is not in items
 
 print(binary_search(4, [1,3,4,5,5,48,450]))
 
